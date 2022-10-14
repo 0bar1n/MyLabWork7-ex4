76ccb7fd3a72f4758aa7a96d8fda48dc9fe5c37c
barinskaya <1388zxc@gmail.com>
2022-10-14 16:17:29 +0300

    Добавьте файлы проекта.

---
+++ MyLabWork7-ex4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyLabWork7_ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y; //переменные вещественного типа
+            x = 3.4; //задние переменной из условия
+            y = Math.Log(Math.Pow(Math.Abs(1-Math.Sqrt(Math.Sin(x))),2)) - (2 / Math.Exp(x)); //задание переменной по формуле
+            Console.WriteLine($"при х={x}, y={y}"); //вывод на экран
+        }
+    }
+}
